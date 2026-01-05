8eae309e746091f775bf81bc400819ae7232c6af
Jurriaan van den Berg <jurriaanvdberg@gmail.com>
2026-01-05 17:18:51 +0100

    Link windows dependencies

--- build.zig
+++ build.zig
@@ -20,24 +20,28 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    lib.root_module.addCSourceFile(.{ .file = upstream.path("src/SDL_net.c") });
-    lib.root_module.addIncludePath(upstream.path("include"));
-    lib.installHeadersDirectory(upstream.path("include/SDL3_net"), "SDL3_net", .{});
-
-    if (lib.linkage.? == .dynamic) {
-        lib.setVersionScript(upstream.path("src/SDL_net.sym"));
-        lib.linker_allow_undefined_version = true;
-        if (target.result.os.tag == .windows) {
-            lib.addWin32ResourceFile(.{ .file = upstream.path("src/version.rc") });
-        }
-    }
-
     const sdl_dep = b.dependency("sdl", .{
         .target = target,
         .optimize = optimize,
         .preferred_linkage = preferred_linkage,
     });
     lib.root_module.linkLibrary(sdl_dep.artifact("SDL3"));
+
+    if (target.result.os.tag == .windows) {
+        lib.root_module.linkSystemLibrary("iphlpapi", .{});
+        lib.root_module.linkSystemLibrary("ws2_32", .{});
+    }
+
+    lib.root_module.addCSourceFile(.{ .file = upstream.path("src/SDL_net.c") });
+    lib.root_module.addIncludePath(upstream.path("include"));
+    lib.installHeadersDirectory(upstream.path("include/SDL3_net"), "SDL3_net", .{});
+
+    if (lib.linkage.? == .dynamic) {
+        lib.setVersionScript(upstream.path("src/SDL_net.sym"));
+        if (target.result.os.tag == .windows) {
+            lib.addWin32ResourceFile(.{ .file = upstream.path("src/version.rc") });
+        }
+    }
 
     b.installArtifact(lib);
 }
