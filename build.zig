825dcc61c45fa65cf80c9c54814b7e8c40a24245
Jurriaan van den Berg <jurriaanvdberg@gmail.com>
2026-01-05 15:14:05 +0100

    Add preferred linkage option

--- build.zig
+++ build.zig
@@ -3,10 +3,16 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const preferred_linkage = b.option(
+        std.builtin.LinkMode,
+        "preferred_linkage",
+        "Prefer building statically or dynamically linked libraries (default: static)",
+    ) orelse .static;
 
     const upstream = b.dependency("sdl_net", .{});
     const lib = b.addLibrary(.{
         .name = "sdl_net",
+        .linkage = preferred_linkage,
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
@@ -21,6 +27,7 @@ pub fn build(b: *std.Build) void {
     const sdl_dep = b.dependency("sdl", .{
         .target = target,
         .optimize = optimize,
+        .preferred_linkage = preferred_linkage,
     });
     lib.root_module.linkLibrary(sdl_dep.artifact("SDL3"));
 
