4d4156488758e8b266dd9f1fa291e926f40cf105
Jurriaan van den Berg <jurriaanvdberg@gmail.com>
2026-05-05 16:11:19 +0200

    Added sysroot support

--- build.zig
+++ build.zig
@@ -4,17 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const build_sdl = b.option(
-        bool,
-        "build_sdl",
-        "Also build and link SDL itself (default: false)",
-    ) orelse false;
-
     const preferred_linkage = b.option(
         std.builtin.LinkMode,
         "preferred_linkage",
         "Prefer building statically or dynamically linked libraries (default: static)",
     ) orelse .static;
+
+    var system_include_path = b.option(
+        std.Build.LazyPath,
+        "system_include_path",
+        "System header search path for cross-compiling",
+    );
+
+    const build_sdl = b.option(
+        bool,
+        "build_sdl",
+        "Also build and link SDL itself (default: false)",
+    ) orelse false;
 
     const upstream = b.dependency("sdl_net", .{});
     const lib = b.addLibrary(.{
@@ -31,20 +37,34 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .preferred_linkage = preferred_linkage,
+        .system_include_path = system_include_path,
     });
+
     if (build_sdl) {
         lib.root_module.linkLibrary(sdl_dep.artifact("SDL3"));
         lib.installHeadersDirectory(sdl_dep.path("include/SDL3"), "SDL3", .{});
     } else lib.root_module.addIncludePath(sdl_dep.path("include"));
 
-    if (target.result.os.tag == .windows) {
-        lib.root_module.linkSystemLibrary("iphlpapi", .{});
-        lib.root_module.linkSystemLibrary("ws2_32", .{});
+    switch (target.result.os.tag) {
+        .windows => {
+            lib.root_module.linkSystemLibrary("iphlpapi", .{});
+            lib.root_module.linkSystemLibrary("ws2_32", .{});
+        },
+        .emscripten, .macos => {
+            if (b.sysroot) |sysroot| {
+                system_include_path = system_include_path orelse .{ .cwd_relative = b.pathJoin(&.{ sysroot, "include" }) };
+            }
+        },
+        else => {},
     }
 
     lib.root_module.addCSourceFile(.{ .file = upstream.path("src/SDL_net.c") });
     lib.root_module.addIncludePath(upstream.path("include"));
     lib.installHeadersDirectory(upstream.path("include/SDL3_net"), "SDL3_net", .{});
+
+    if (system_include_path) |path| {
+        lib.root_module.addSystemIncludePath(path);
+    }
 
     if (lib.linkage.? == .dynamic) {
         lib.setVersionScript(upstream.path("src/SDL_net.sym"));
